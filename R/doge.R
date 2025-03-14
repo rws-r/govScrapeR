6226d7c60b5d0f28af6abfabfd7e0d12dd3d37f4
rws-r <brightoctoberdreams@gmail.com>
2025-03-13 23:43:28 -0500

    Code cleanup and relocation

--- R/doge.R
+++ R/doge.R
@@ -1,29 +1,32 @@
+#' DOGE Get Data
+#'
+#' A fairly straightforward scraper that pulls data from dynamically-created
+#' scripts (javascript), cleans, and processes in table form.
+#'
+#' @param verbose
+#'
+#' @importFrom rvest read_html
+#' @importFrom rvest html_nodes
+#' @importFrom rvest html_text
+#' @importFrom stringr str_extract
+#' @importFrom jsonlite fromJSON
+#' @importFrom dplyr  mutate
+#' @importFrom dplyr %>% 
+#' @importFrom dplyr rename
+#' @returns A named list comprised of dataframes.
+#' @export
+#'
+#' @examples get_doge_data()
+#' 
+doge_get_data <- function(verbose=TRUE){
 
-##--------DOGE Functions
-get_doge_receipts_tables <- function(){
-  require(jsonlite)
-  require(dplyr)
-  
-  url <- "https://doge.gov/api/receipts/overview"
-  json <- fromJSON(url)
-  
-  return(json)
-  
-}
-
-get_doge_scripts <- function(verbose=TRUE){
-  library(rvest)
-  library(jsonlite)
-  library(stringr)
-  library(lubridate)
-  
   url <- "https://www.doge.gov/savings"
   
   if(verbose==TRUE)message("Fetching url...")
   page <- rvest::read_html(url)
   
   if(verbose==TRUE)message("Capturing elements...")
-  scripts <- page %>% html_nodes("script") %>% html_text()
+  scripts <- page %>% rvest::html_nodes("script") %>% rvest::html_text()
   ## Cut out final function scripts
   scripts <- scripts[-length(scripts)]
   ## Get NAs out.
@@ -31,7 +34,7 @@ get_doge_scripts <- function(verbose=TRUE){
   
   if(verbose==TRUE)message("Cleaning raw data...")
   scripts <- paste(scripts,collapse="")
-  scripts <- str_extract(scripts, '\\\\"receipts\\\\\\":.*')
+  scripts <- stringr::str_extract(scripts, '\\\\"receipts\\\\\\":.*')
   
   ## Get some data cleaning done, based on the garbage provided.
   scripts <- gsub("\\\"]\\)self\\.\\_\\_next\\_f\\.push\\(\\[1,\\\"","",scripts)
@@ -61,41 +64,41 @@ get_doge_scripts <- function(verbose=TRUE){
   
   if(verbose==TRUE)message("Dataframe cleaning...")
   contracts <- suppressWarnings(contracts %>% 
-                                  rename(award_id_piid = piid) %>% 
-                                  mutate(date=ifelse(date=="",NA,date),
-                                         date=case_when(
-                                           grepl("-", date) ~ as.Date(date,format = "%Y-%m-%d"),
-                                           grepl("\\/",date) ~ mdy(date),
-                                           .default=NA),
-                                         ceiling_value=as.numeric(ceiling_value),
-                                         value=as.numeric(value),
-                                         update_date=ifelse(update_date=="",NA,update_date),
-                                         update_date=case_when(
-                                           grepl("-", update_date) ~ as.Date(update_date,format = "%Y-%m-%d"),
-                                           grepl("\\/",update_date) ~ mdy(update_date),
-                                           .default=NA),
-                                         savings=value))
+                                  dplyr::rename(award_id_piid = piid) %>% 
+                                  dplyr::mutate(date=ifelse(date=="",NA,date),
+                                                date=case_when(
+                                                  grepl("-", date) ~ as.Date(date,format = "%Y-%m-%d"),
+                                                  grepl("\\/",date) ~ mdy(date),
+                                                  .default=NA),
+                                                ceiling_value=as.numeric(ceiling_value),
+                                                value=as.numeric(value),
+                                                update_date=ifelse(update_date=="",NA,update_date),
+                                                update_date=case_when(
+                                                  grepl("-", update_date) ~ as.Date(update_date,format = "%Y-%m-%d"),
+                                                  grepl("\\/",update_date) ~ mdy(update_date),
+                                                  .default=NA),
+                                                savings=value))
   
-  leases <- suppressWarnings(leases %>% mutate(date=ifelse(date=="",NA,date),
-                                               date=case_when(
-                                                 grepl("-", date) ~ as.Date(date,format = "%Y-%m-%d"),
-                                                 grepl("\\/",date) ~ mdy(date),
-                                                 .default=NA),
-                                               ceiling_value=as.numeric(ceiling_value),
-                                               value=as.numeric(value),
-                                               sq_ft=gsub(",","",sq_ft),
-                                               savings=value
+  leases <- suppressWarnings(leases %>% dplyr::mutate(date=ifelse(date=="",NA,date),
+                                                      date=case_when(
+                                                        grepl("-", date) ~ as.Date(date,format = "%Y-%m-%d"),
+                                                        grepl("\\/",date) ~ mdy(date),
+                                                        .default=NA),
+                                                      ceiling_value=as.numeric(ceiling_value),
+                                                      value=as.numeric(value),
+                                                      sq_ft=gsub(",","",sq_ft),
+                                                      savings=value
   ))
   
-  grants <- suppressWarnings(grants %>% mutate(update_date=ifelse(update_date=="",NA,update_date),
-                                               #   award_id = gsub("-M*","",award_id), # Why did this strip this out??
-                                               update_date=case_when(
-                                                 grepl("-", update_date) ~ as.Date(update_date,format = "%Y-%m-%d"),
-                                                 grepl("\\/",update_date) ~ mdy(update_date),
-                                                 .default=NA),
-                                               ceiling_value=as.numeric(ceiling_value),
-                                               value=as.numeric(value),
-                                               savings=value
+  grants <- suppressWarnings(grants %>% dplyr::mutate(update_date=ifelse(update_date=="",NA,update_date),
+                                                      #   award_id = gsub("-M*","",award_id), # Why did this strip this out??
+                                                      update_date=case_when(
+                                                        grepl("-", update_date) ~ as.Date(update_date,format = "%Y-%m-%d"),
+                                                        grepl("\\/",update_date) ~ mdy(update_date),
+                                                        .default=NA),
+                                                      ceiling_value=as.numeric(ceiling_value),
+                                                      value=as.numeric(value),
+                                                      savings=value
   ))
   
   contracts_url_elements <- extract_url_components(contracts)
@@ -114,34 +117,20 @@ get_doge_scripts <- function(verbose=TRUE){
   
 }
 
-extract_url_components <- function(data=NULL,
-                                   url_col="fpds_link",
-                                   link=NULL){
-  if(!is.null(link)){
-    links <- link
-  }else{
-    links <- data[,url_col]
-  }
-  for(i in 1:length(links)){
-    args <- strsplit(links[i],"\\?")[[1]][2]
-    elements <- strsplit(args,"&")[[1]]
-    vals <- strsplit(elements,"=")
-    if(i==1)
-      x <- as.data.frame(matrix(NA,ncol = length(vals)))
-    for(j in 1:length(vals)){
-      x[1,j] <- vals[[j]][2]
-      colnames(x)[j] <- vals[[j]][1]
-    }
-    if(i==1)
-      y <- x
-    else
-      y <- dplyr::bind_rows(y,x)
-  }
-  return(y)
-}
-
-summarize_doge_receipts <- function(data,return="all"){
-  require(scales)
+#' DOGE Summarize
+#' 
+#' Present basic stats on the DOGE data scraped.
+#'
+#' @param data 
+#' @param return 
+#'
+#' @returns A printed data.frame.
+#' @export
+#'
+#' @examples doge_summarize(data=x)
+#' 
+doge_summarize <- function(data=NULL,
+                           return="all"){
   
   c <- data$contracts
   l <- data$leases
@@ -248,122 +237,3 @@ summarize_doge_receipts <- function(data,return="all"){
   }
 }
 
-recursive_test <- function(list,val=NULL){
-  for(i in 1:length(list)){
-    if(inherits(list[[i]],"list")){
-      val <- recursive_test(list[[i]],val)
-    }else{
-      val <- c(val,list[[i]])
-    }
-  }
-  return(val)
-}
-
-get_workforce_data <- function(id=NULL){
-  base_office_url <- "https://doge.gov/api/offices/"
-  l <- 0
-  
-  if(is.null(id))
-    top_id <- "69ee18bc-9ac8-467e-84b0-106601b01b90"
-  else
-    top_id <- id
-  
-  jsoncall <- function(id){
-    x <- fromJSON(paste0(base_office_url,id))
-    return(x)
-  }
-  
-  build_df <- function(t,level){
-    d <- data.frame(parent = ifelse(is.null(t$office$parentId),NA,t$office$parentId),
-                    name = ifelse(is.null(t$office$name),NA,t$office$name),
-                    id = ifelse(is.null(t$office$id),NA,t$office$id),
-                    fedscopediv = ifelse(is.null(t$office$fedscopeDivision),NA,t$office$fedscopeDivision),
-                    childcount = ifelse(is.null(t$office$childCount),NA,t$office$childCount),
-                    level = level)
-    return(d)
-  }
-  
-  ## Recursive function
-  get_kids <- function(id,
-                       dataframe=NULL,
-                       pid=NULL,
-                       level=l){
-    
-    j <- jsoncall(id)
-    
-    df <- build_df(j,level=level)
-    dataframe <- rbind(dataframe,df)
-    
-    kids <- j$children
-    
-    if(inherits(kids,"list")){
-      # message(paste("No children, getting ",j$office$id))
-      # df <- build_df(j,level=level)
-      # dataframe <- rbind(dataframe,df)
-    }else if(inherits(kids,"data.frame")){
-      message("Getting children:")
-      
-      for(i in seq_along(kids$id)){
-        message(kids$id[i])
-        dataframe <- get_kids(kids$id[i],dataframe=dataframe,level=(level+1))
-      }
-    }else{
-      stop("ERROR")
-    }
-    return(dataframe)
-  }
-  
-  dataframe <- data.frame(parent = character(), 
-                          name = character(), 
-                          id = character(), 
-                          fedscopediv = character(), 
-                          childcount = integer(), 
-                          level = integer(), 
-                          stringsAsFactors = FALSE)
-  
-  d <- get_kids(top_id,dataframe = dataframe,level = l)
-  
-  return(d)
-}
-
-get_workforce_data_kpis <- function(workforce_data){
-  require(jsonlite)
-  require(tidyr)
-  require(dplyr)
-  
-  pb <- txtProgressBar(min = 0,max=nrow(workforce_data),style = 3)
-  for(i in 1:nrow(workforce_data)){
-    url <- paste0("https://doge.gov/api/kpis/",workforce_data[i,"id"])
-    
-    j <- tryCatch({
-      print(url)
-      j <- jsonlite::fromJSON(url,flatten = T)
-    }, error = function(e) {
-      message("Error on URL ", url, ": ", e$message)
-      j <- as.data.frame(matrix(NA,nrow=1,ncol=length(names(df))))
-      colnames(j) <- names(df)
-      j$office_id <- workforce_data[i,"id"]
-      return(j)  # Return NULL on error and continue loop
-    })
-    
-    # j <- jsonlite::fromJSON(url,flatten = T)
-    
-    if(!is.null(j)){
-      
-      ## Handle potential empty entries
-      if(length(j$detailed)==0)
-        j$detailed <- list(NA)
-      
-      j <- as.data.frame(j)
-      
-      if(i==1)
-        df <- j
-      else
-        df <- dplyr::bind_rows(df,j)
-    }
-    
-    setTxtProgressBar(pb,i)
-  }
-  close(pb)
-  return(df)
-}
