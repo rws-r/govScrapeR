8399ad12a56fe98768293b6c781cdba01de304ca
rws-r <brightoctoberdreams@gmail.com>
2025-03-26 08:01:03 -0500

    Update DOGE logics to implement new API. Cleaned misc functions.

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,3 @@
 # govScrapeR 0.0.1
 
-* Starting NEWS for updates. 
+* 2025-03-26. doge.gov/savings finally implemented an api; did another rewrite to accomodate. Bumping up to 0.0.1 version as well. 
--- tests/testthat/test-doge_get_data.R
+++ tests/testthat/test-doge_get_data.R
@@ -1,11 +1,12 @@
 test_that("doge_get_data() works properly", {
   d <- doge_get_data(verbose=F)
   
-  expect_length(d,3)
+  expect_length(d,4)
   expect_s3_class(d$contracts,"data.frame")
   expect_s3_class(d$grants,"data.frame")
   expect_s3_class(d$leases,"data.frame")
+  expect_s3_class(d$payments,"data.frame")
   
-  expect_output(doge_summarize(d))
+  #expect_output(doge_summarize(d))
 })
 
